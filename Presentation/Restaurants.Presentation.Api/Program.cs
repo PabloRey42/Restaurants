2bbe3a0b20e60d7c714b901054cf85e206be512e
REYPablo <pablo.reyvarenne@laposte.net>
2026-06-01 13:37:39 +0200

    Config Docker Hub et compose pour CI/CD

--- Presentation/Restaurants.Presentation.Api/Program.cs
+++ Presentation/Restaurants.Presentation.Api/Program.cs
@@ -4,44 +4,39 @@ using Restaurants.Infrastructure.Database;
 using Restaurants.Infrastructure.Ioc;
 using Restaurants.Presentation.Api;
 using Serilog;
-
 try
 {
     var builder = WebApplication.CreateBuilder(args);
-
     builder.Services.AddInfrastructure(builder.Configuration);
     builder.Services.AddApplication();
     builder.AddPresentation();
-
-
     var app = builder.Build();
-    var scope = app.Services.CreateAsyncScope();
-    var seeder = scope.ServiceProvider.GetService<IRestaurantsSeeder>();
-    if (seeder != null)
+
+    // Le scope de seeding est isolé dans un using : il est libéré
+    // (et sa connexion DB fermée) AVANT le démarrage du serveur web.
+    await using (var scope = app.Services.CreateAsyncScope())
     {
-        await seeder.SeedIdentityAsync();
-        await seeder.SeedRestaurantAsync();
+        var seeder = scope.ServiceProvider.GetService<IRestaurantsSeeder>();
+        if (seeder != null)
+        {
+            await seeder.SeedIdentityAsync();
+            await seeder.SeedRestaurantAsync();
+        }
     }
 
     app.UseSerilogRequestLogging();
-
     app.UseMiddleware<ExceptionHandlerMiddleware>();
-
 // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
     }
-
     app.UseHttpsRedirection();
-
     app.UseAuthorization();
     app.UseCors("AllowSpecificOrigin");
-
     app.MapControllers();
     app.MapGroup("api/Identities").MapIdentityApi<User>();
-
     await app.RunAsync();
 }
 catch (Exception exception)
@@ -52,7 +47,6 @@ finally
 {
     Log.CloseAndFlush();
 }
-
 /// <summary>
 ///     Class partiel du program pour l'initier dans les tests
 /// </summary>
